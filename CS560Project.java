96674be0d44a0daa4fb9c06641a072ea5dd8fb69
jcouri <josephmcouri@gmail.com>
2019-01-14 22:05:00 -0800

    CS560Project

--- CS560Project.java
+++ CS560Project.java
@@ -1,31 +1,282 @@
-package FinalProject;
+package groupProgramAssignment;
 
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
 
 public class BestPath {
-	private Node list[];
-	private Node queue[];
-	private Node stack[];
-	int head;
-	int tail;
-	int top;
+	private Node[] hive = new Node[234];
+	private Node[] queue = new Node[1000];
+	private Node[] stack = new Node[233];
+	int head = 0;
+	int tail = 0;
+	int top = 0;
 	
 	public BestPath() {
-		list = new Node [234];
-		queue = new Node [1000];
-		stack = new Node [234];
-		head = 0;
-		tail = 0;
-		top = 0;
-		read();
+		readIn();
 		setup(226);
-		enqueue(list[226]);
-		list[226].total=list[226].cost;
+		enqueue(hive[226]);
+		hive[226].total = hive[226].cost;
 		mark();
-		move(list[8]);
-		print();
+		move(hive[8]);
+		pprint();
+	}
+	
+	/**
+	 * Reads in the data from the file and puts each node into an array.
+	 */
+	public void readIn() {
+		try {
+			BufferedReader in = new BufferedReader(new FileReader("./src/groupProgramAssignment/input.txt"));
+			String line;
+			int j;
+			for(int i = 1; i <= 233; i++) {
+				line = in.readLine();
+				String[] split = line.split("\\s+");
+				j = Integer.parseInt(split[0]);
+				hive[j] = new Node(j, Integer.parseInt(split[1]));
+			}
+			in.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	/**
+	 * Connects the nodes together starting at the bottom-left corner, spreading up and right by assigning the top, top-right, and bottom-right nodes to the bottom, bottom-left, and top-left of each of the adjacent nodes in tose places.
+	 * @param i
+	 */
+	public void setup(int i) {
+		int j = i - 7;
+		boolean b = i % 15 != 8;
+		if(b && i - 8 > 0 && hive[i].tr == null) {
+			hive[i].tr = hive[j];
+			hive[i].tr.bl = hive[i];
+			setup(j);
+		}
+		j = i - 15;
+		if(j > 0 && hive[i].t == null) {
+			hive[i].t = hive[j];
+			hive[i].t.b = hive[i];
+			setup(j);
+		}
+		j = i + 8;
+		if(b && j <= 233 && hive[i].br == null) {
+			hive[i].br = hive[j];
+			hive[i].br.tl = hive[i];
+			setup(j);
+		}
+	}
+	
+	public void enqueue(Node n){
+		queue[tail] = n;
+		tail++;
+	}
+	
+	public Node dequeue(){
+		if (head != tail){
+			Node n = queue[head];
+			head++;
+			return n;
+		}
+		return null;
+	}
+	
+	/**
+	 * Checks if each adjacent node is present, doesn't cost -1, and did not previously have its total cost evaluated.
+	 * Only then will it enqueue the next node.
+	 */
+	public void mark() {
+		Node n = dequeue();
+		if (n == null) 
+			return;		
+		if(n.t != null) {
+			if(n.t.cost != -1) {
+				n.toT = n.total + n.t.cost;
+				if(n.toT < n.t.total) {
+					n.t.total = n.toT;
+					enqueue(n.t);
+				}
+			}
+		}
+		if(n.tr != null) {
+			if(n.tr.cost != -1) {
+				n.toTR = n.total + n.tr.cost;
+				if(n.toTR < n.tr.total) {
+					n.tr.total = n.toTR;
+					enqueue(n.tr);
+				}
+			}
+		}
+		if(n.br != null) {
+			if(n.br.cost != -1) {
+				n.toBR = n.total + n.br.cost;
+				if(n.toBR < n.br.total) {
+					n.br.total = n.toBR;
+					enqueue(n.br);
+				}
+			}
+		}
+		if(n.b != null) {
+			if(n.b.cost != -1) {
+				n.toB = n.total + n.b.cost;
+				if(n.toB < n.b.total) {
+					n.b.total = n.toB;
+					enqueue(n.b);
+				}
+			}
+		}
+		if(n.bl != null) {
+			if(n.bl.cost != -1) {
+				n.toBL = n.total + n.bl.cost;
+				if(n.toBL < n.bl.total) {
+					n.bl.total = n.toBL;
+					enqueue(n.bl);
+				}
+			}
+		}
+		if(n.tl != null){
+			if(n.tl.cost != -1){
+				n.toTL = n.total + n.tl.cost;
+				if(n.toTL < n.tl.total) {
+					n.tl.total = n.toTL;
+					enqueue(n.tl);
+				}
+			}
+		}
+		mark();
+	}
+	
+	public void push(Node n) {
+		stack[top] = n;
+		top++;
+	}
+	
+	public Node pop() {
+		top--;
+		return stack[top];
+	}
+	
+	/**
+	 * Checks if each adjacent node is present, doesn't cost -1, and has not been visited already.
+	 * Only then does it move to the node with the smallest evaluated cost.
+	 * @param n
+	 */
+	public void move(Node n) {
+		push(n);
+		n.visited = true;
+		if(n.t != null) {
+			if(n.t.cost != -1) {
+				if(!n.t.visited) {
+					if(n.t.total < n.minCost) {
+						n.minCost = n.t.total;
+						n.minPath = 0;
+					}
+				}
+			}
+		}
+		if(n.tr != null) {
+			if(n.tr.cost != -1) {
+				if(!n.tr.visited) {
+					if(n.tr.total < n.minCost) {
+						n.minCost = n.tr.total;
+						n.minPath = 1;
+					}
+				}
+			}
+		}
+		if(n.br != null) {
+			if(n.br.cost != -1) {
+				if(!n.br.visited) {
+					if(n.br.total < n.minCost) {
+						n.minCost = n.br.total;
+						n.minPath = 2;
+					}
+				}
+			}
+		}
+		if(n.b != null) {
+			if(n.b.cost!=-1) {
+				if(!n.b.visited) {
+					if(n.b.total < n.minCost) {
+						n.minCost = n.b.total;
+						n.minPath = 3;
+					}
+				}
+			}
+		}
+		if(n.bl != null) {
+			if(n.bl.cost != -1) {
+				if(!n.bl.visited) {
+					if(n.bl.total < n.minCost) {
+						n.minCost = n.bl.total;
+						n.minPath = 4;
+					}
+				}
+			}
+		}
+		if(n.tl != null) {
+			if(n.tl.cost != -1) {
+				if(!n.tl.visited) {
+					if(n.tl.total < n.minCost) {
+						n.minCost = n.tl.total;
+						n.minPath = 5;
+					}
+				}
+			}
+		}
+		switch(n.minPath) {
+			case -1:
+				break;
+			case 0:
+				move(n.t);
+				break;
+			case 1:
+				move(n.tr);
+				break;
+			case 2:
+				move(n.br);
+				break;
+			case 3:
+				move(n.b);
+				break;
+			case 4:
+				move(n.bl);
+				break;
+			case 5:
+				move(n.tl);
+		}
+	}
+	
+	public void pprint(){
+		try {
+			PrintWriter out = new PrintWriter("./src/groupProgramAssignment/output.txt");
+			int i;
+			while(top > 0) {
+				i = pop().index;
+				out.println(i);
+				System.out.println(i);
+			}
+			String s = "MINIMAL-COST PATH COSTS: " + hive[8].total;
+			out.println(s);
+			System.out.println(s);
+			out.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		long time = System.nanoTime();
+		new BestPath();
+		System.out.println((float) (System.nanoTime() - time) / 1E9 + " seconds");
 	}
 	
 	class Node {
@@ -41,270 +292,22 @@ public class BestPath {
 		int toBL;
 		int toTL;
 		boolean visited;
-		Node T;
-		Node B;
-		Node TL;
-		Node TR;
-		Node BL;
-		Node BR;
+		Node t;
+		Node b;
+		Node tl;
+		Node tr;
+		Node bl;
+		Node br;
+		
 		Node(int i, int data) {
 			cost = data;
 			index = i;
-			total = 999999;
-			minCost = 999999;
-			minPath = 9;
+			total = 2147483647;
+			minCost = 2147483647;
+			minPath = -1;
 			visited = false;
-			T = B = TL = TR = BL = BR = null;
+			t = b = tl = tr = bl = br = null;
 		}
-	}
-	
-	public void test(int i) {
-		System.out.println("index: " + list[i].index);
-		if (list[i].T == null){
-			System.out.println("T: null");
-		}
-		else System.out.println("T: " + list[i].T.index);
-		if (list[i].TR == null){
-			System.out.println("TR: null");
-		}
-		else System.out.println("TR: " + list[i].TR.index);
-		if (list[i].BR == null){
-			System.out.println("BR: null");
-		}
-		else System.out.println("BR: " + list[i].BR.index);
-		if (list[i].B == null){
-			System.out.println("B: null");
-		}
-		else System.out.println("B: " + list[i].B.index);
-		if (list[i].BL == null){
-			System.out.println("BL: null");
-		}
-		else System.out.println("BL: " + list[i].BL.index);
-		if (list[i].TL == null){
-			System.out.println("TL: null");
-		}
-		else System.out.println("TL: " + list[i].TL.index);
-	}
-	
-	public void test2(int i) {
-		while (i<top){
-			System.out.println(stack[i].index);
-			i++;
-		}
-	}
-	
-	public void read() {
-		String line;
-    	try{
-    		BufferedReader in = new BufferedReader(new FileReader("INPUT.TXT"));
-    		while((line = in.readLine()) != null){
-    			String value = line;
-    			String [] split = value.split("\\s+");
-    			int index = Integer.parseInt(split[0]);
-    			int cost = Integer.parseInt(split[1]);
-    			add(index, cost);
-    		}
-    		in.close();
-    	}
-    	catch(IOException e){
-    		System.out.println("Cannot find file");
-    	}
-	}
-	
-	public void add(int i, int data) { 
-		Node newNode = new Node(i, data);
-		list[i] = newNode;
-	}
-	
-	public void setup(int i) {
-		if (list[i].index % 15 != 8 && list[i].index-8 > 0 && list[i].TR==null) {
-			list[i].TR = list[list[i].index-7];
-			list[list[i].index-7].BL = list[i];
-			setup(list[i].index-7);
-		}
-		if (list[i].index - 15 > 0 && list[i].T==null) {
-			list[i].T = list[list[i].index-15];
-			list[list[i].index-15].B = list[i];
-			setup(list[i].index-15);
-		}
-		if (list[i].index % 15 != 8 && list[i].index + 8 <= 233 &&list[i].BR==null) {
-			list[i].BR = list[list[i].index+8];
-			list[list[i].index+8].TL = list[i];
-			setup(list[i].index+8);
-		}
-	}
-	
-	public void mark() {
-		Node N = dequeue();
-		if (N == null) return;
-		if(N.T!=null){
-			if (N.T.cost!=-1){
-				if(N.T.total==999999) enqueue(N.T);
-				N.toT = N.total + N.T.cost;
-				if(N.toT<N.T.total){
-					N.T.total=N.toT;
-					enqueue(N.T);
-				}
-			}
-		}
-		if(N.TR!=null){
-			if(N.TR.cost!=-1){
-				if(N.TR.total==999999) enqueue(N.TR);
-				N.toTR = N.total + N.TR.cost;
-				if(N.toTR<N.TR.total){
-					N.TR.total=N.toTR;
-					enqueue(N.TR);
-				}
-			}
-		}
-		if(N.BR!=null){
-			if (N.BR.cost!=-1){
-				if(N.BR.total==999999) enqueue(N.BR);
-				N.toBR = N.total + N.BR.cost;
-				if(N.toBR<N.BR.total){
-					N.BR.total=N.toBR;
-					enqueue(N.BR);
-				}
-			}
-		}
-		if(N.B!=null){
-			if (N.B.cost!=-1){
-				if(N.B.total==999999) enqueue(N.B);
-				N.toB = N.total + N.B.cost;
-				if(N.toB<N.B.total){
-					N.B.total=N.toB;
-					enqueue(N.B);
-				}
-			}
-		}
-		if(N.BL!=null){
-			if (N.BL.cost!=-1){
-				if(N.BL.total==999999) enqueue(N.BL);
-				N.toBL = N.total + N.BL.cost;
-				if(N.toBL<N.BL.total){
-					N.BL.total=N.toBL;
-					enqueue(N.BL);
-				}
-			}
-		}
-		if(N.TL!=null){
-			if (N.TL.cost!=-1){
-				if(N.TL.total==999999) enqueue(N.TL);
-				N.toTL = N.total + N.TL.cost;
-				if(N.toTL<N.TL.total){
-					N.TL.total=N.toTL;
-					enqueue(N.TL);
-				}
-			}
-		}
-		mark();
-	}
-	
-	public void move(Node N) {
-		push(N);
-		N.visited=true;
-		if(N.T!=null){
-			if (N.T.cost!=-1){
-				if (!N.T.visited){
-					if (N.T.total<N.minCost){
-						N.minCost=N.T.total;
-						N.minPath=0;
-					}
-				}
-			}
-		}
-		if(N.TR!=null){
-			if (N.TR.cost!=-1){
-				if (!N.TR.visited){
-					if (N.TR.total<N.minCost){
-						N.minCost=N.TR.total;
-						N.minPath=1;
-					}
-				}
-			}
-		}
-		if(N.BR!=null){
-			if (N.BR.cost!=-1){
-				if (!N.BR.visited){
-					if (N.BR.total<N.minCost){
-						N.minCost=N.BR.total;
-						N.minPath=2;
-					}
-				}
-			}
-		}
-		if(N.B!=null){
-			if (N.B.cost!=-1){
-				if (!N.B.visited){
-					if (N.B.total<N.minCost){
-						N.minCost=N.B.total;
-						N.minPath=3;
-					}
-				}
-			}
-		}
-		if(N.BL!=null){
-			if (N.BL.cost!=-1){
-				if (!N.BL.visited){
-					if (N.BL.total<N.minCost){
-						N.minCost=N.BL.total;
-						N.minPath=4;
-					}
-				}
-			}
-		}
-		if(N.TL!=null){
-			if (N.TL.cost!=-1){
-				if (!N.TL.visited){
-					if (N.TL.total<N.minCost){
-						N.minCost=N.TL.total;
-						N.minPath=5;
-					}
-				}
-			}
-		}
-		if (N.minPath==0) move(N.T); 
-		if (N.minPath==1) move(N.TR);
-		if (N.minPath==2) move(N.BR);
-		if (N.minPath==3) move(N.B);
-		if (N.minPath==4) move(N.BL);
-		if (N.minPath==5) move(N.TL);
-	}
-	
-	public void enqueue(Node N){
-		queue[tail] = N;
-		tail++;
-	}
-	
-	public Node dequeue(){
-		if (head!=tail){
-			Node N = queue[head];
-			head++;
-			return N;
-		}
-		return null;
-	}
-	
-	public void push(Node N){
-		stack[top] = N;
-		top++;
-	}
-	
-	public Node pop(){
-		top--;
-		Node N = stack[top];
-		return N;
-	}
-	
-	public void print(){
-		while(top!=0){
-			System.out.println(pop().index);
-		}
-		System.out.println("MINIMAL-COST PATH COSTS: " + list[8].total);
-	}
-
-	public static void main(String[] args) {
-		new BestPath();
 	}
 
 }
